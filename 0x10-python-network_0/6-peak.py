a2368577056443684e991c3dfcdbf5662340303e
Creationdis <creationdis@gmail.com>
2023-09-28 16:48:36 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python3
-"""This is the peak module."""
+"""Finds peak in unsorted list of integers"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers.
-
-    Args:
-        list_of_integers (list): the list of integers to find the peak in.
-    """
-
-    if list_of_integers == []:
+    """Finds peak in unsorted list of integers"""
+    if type(list_of_integers) is not list:
+        return
+    if len(list_of_integers) == 0:
         return None
-
-    length = len(list_of_integers)
-    if length == 1:
-        return list_of_integers[0]
-    if length == 2:
-        return max(list_of_integers)
-
-    half = int(length / 2)
-    peak = list_of_integers[half]
-    if peak > list_of_integers[half - 1] and peak > list_of_integers[half + 1]:
-        return peak
-    elif peak > list_of_integers[half + 1]:
-        return find_peak(list_of_integers[:half])
-    else:
-        return find_peak(list_of_integers[half + 1:])
+    list_of_integers.sort(reverse=True)
+    return list_of_integers[0]
